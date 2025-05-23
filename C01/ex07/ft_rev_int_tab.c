e37d8f68610b76105a1d44ae138ef92a06b38ba2
FavRedX <redouaneoverpower@email.com>
2025-05-23 15:01:49 +0100

    Added

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,13 @@
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	tmp;
+
+	i = -1;
+	while (++i < size / 2)
+	{
+		tmp = tab[i];
+		tab[i] = tab[size - i - 1];
+		tab[size - i - 1] = tmp;
+	}
+}

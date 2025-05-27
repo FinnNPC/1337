514eaae9e341c6145b9538cc01fe6f36aae21d4b
FavRedX <redouaneoverpower@email.com>
2025-05-27 05:37:26 +0100

    Done

---
+++ C02/ex08/ft_strlowcase.c
@@ -0,0 +1,16 @@
+char *ft_strupcase(char *str)
+{
+	int i;
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+		{	
+			str[i] = str[i] + 32;
+		}
+		i++;
+	}
+
+	return str;
+}
